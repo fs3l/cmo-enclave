618eb66e83496d4dda8dd334f378de32f7868397
Xu Cheng <git@xuc.me>
2017-11-24 15:08:13 -0500

    use struct

--- include/cmo.h
+++ include/cmo.h
@@ -7,9 +7,8 @@ class ObIterator;
 class ObRwIterator;
 class NobArray;
 
-class CMO
+struct CMO
 {
-public:
   ObIterator init_ob_iterator(int32_t *data, int32_t len);
   ObRwIterator init_ob_rw_iterator(int32_t *data, int32_t len);
   NobArray init_nob_array(int32_t *data, int32_t len);
@@ -20,27 +19,23 @@ public:
   friend class ObRwIterator;
   friend class NobArray;
 
-private:
   void begin_tx();
   void end_tx();
 };
 
-class ObIterator
+struct ObIterator
 {
-public:
   int32_t read_next();
 };
 
-class ObRwIterator
+struct ObRwIterator
 {
-public:
   int32_t read_next();
   void write_next(int32_t data);
 };
 
-class NobArray
+struct NobArray
 {
-public:
   int32_t read_at(int32_t addr);
   void write_at(int32_t addr, int32_t data);
 };
