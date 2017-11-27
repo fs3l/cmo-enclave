b13c2e0e5abe3df609d6f1fa1e20bac2be5871d3
Xu Cheng <git@xuc.me>
2017-11-27 13:52:32 -0500

    add more utility functions

--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,30 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <math.h>
+#include <stdint.h>
+
 template <class T>
 inline T min(const T& a, const T& b)
 {
   return a < b ? a : b;
 }
+
+template <class T>
+inline T max(const T& a, const T& b)
+{
+  return a > b ? a : b;
+}
+
+inline void swap(int32_t* a, int32_t* b)
+{
+  int32_t tmp = *a;
+  *b = *a;
+  *a = tmp;
+}
+
+int32_t random_int32();
+
+void fisher_yates_shuffle(int32_t* arr, int32_t len);
 
 #endif
--- src/lib/utils.cpp
+++ src/lib/utils.cpp
@@ -1,1 +1,26 @@
 #include "utils.h"
+
+#include <stdlib.h>
+
+#ifdef SGXAPI
+#include <sgx_trts.h>
+#endif
+
+int32_t random_int32()
+{
+#ifdef SGXAPI
+  int32_t result;
+  sgx_read_rand((unsigned char*)&result, 4);
+  return abs(result);
+#else
+  return rand() % INT32_MAX;
+#endif
+}
+
+void fisher_yates_shuffle(int32_t* arr, int32_t len)
+{
+  for (int32_t i = 0; i < len - 1; ++i) {
+    int32_t j = random_int32() % len;
+    swap(&arr[i], &arr[j]);
+  }
+}
