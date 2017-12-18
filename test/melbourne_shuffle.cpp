4f9193761451a4ab207875f6f7986de9458b0880
Ju Chen <chenju2k6@gmail.com>
2017-12-17 23:44:12 -0500

    ih
    Signed-off-by: Ju Chen <chenju2k6@gmail.com>

--- test/melbourne_shuffle.cpp
+++ test/melbourne_shuffle.cpp
@@ -7,18 +7,23 @@
 #else
 #include "algo.h"
 #endif
+#include <sys/time.h>
 
 BOOST_AUTO_TEST_CASE(melbourne_shuffle_test)
 {
-  //int32_t len = 1048576;
-  int32_t len = 100;
+  int32_t len = 1048576;
+  //int32_t len = 100;
   int32_t* input = gen_random_sequence(len);
   int32_t* output = new int32_t[len];
+  struct timeval begin,end;
+  gettimeofday(&begin,NULL);
 #ifdef SGX_APP
   ecall_melbourne_shuffle(global_eid, input, input, output, len, 1);
 #else
   melbourne_shuffle(input, input, output, len, 1);
 #endif
+  gettimeofday(&end,NULL);
+  printf("time spent=%ld\n",1000000*(end.tv_sec-begin.tv_sec)+end.tv_usec-begin.tv_usec);
   for (int32_t i = 0; i < len; ++i) BOOST_CHECK(output[i] == i);
   delete[] input;
   delete[] output;
